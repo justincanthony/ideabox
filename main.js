0067335f9dd088ed3ee978223961186eb8c16813
Justin Anthony <justincanthony@yahoo.com>
2021-06-03 17:41:00 -0600

    Create query selectors

---
+++ main.js
@@ -0,0 +1,9 @@
+// 👇 querySelectors👇
+var saveButton = document.querySelector('.save-btn');
+var displayStarredButton = document.querySelector('.show-starred-button');
+var deleteIdeaButton = document.querySelector('.')
+var ideaTitle = document.querySelector('.form-title');
+var ideaBody = document.querySelector('.form-body');
+
+// 👇 eventListeners👇
+//
