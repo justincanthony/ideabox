8bb9ee18eee5b3daa2bbc8e0fffa801cc59d8b5f
Matt Roden <mattroden92@gmail.com>
2021-06-05 12:21:13 -0600

    Make function to create new card on save click

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 //👇 querySelectors👇
 var saveButton = document.querySelector('.save-btn');
 var displayStarredButton = document.querySelector('.show-starred-button');
-var addCommentButton = document.querySelector('.comment-button')
+var addCommentButton = document.querySelector('.comment-button');
 // var deleteIdeaButton = document.querySelector('.')
-var ideaTitle = document.querySelector('.form-title');
-var ideaBody = document.querySelector('.form-body');
-var searchText = document.querySelector('.search-field')
+var ideaTitle = document.querySelector('.form-title'); // title text input
+var ideaBody = document.querySelector('.form-body'); // body text input
+var searchText = document.querySelector('.search-field');
+var renderedIdeas = document.querySelector('.ideabox');
 var savedIdeas = [];
-
+console.log(renderedIdeas);
 
 //👇 eventListeners👇
 //
@@ -17,12 +18,39 @@ saveButton.addEventListener('click', invokeIdeaClass);
 
 
 //👇 eventHandlers👇
+//Save card + make new instance of Idea
 function invokeIdeaClass(event) {
   event.preventDefault();
   var newIdeaObject = new Idea(ideaTitle.value, ideaBody.value);
   savedIdeas.push(newIdeaObject);
-  console.log(newIdeaObject);
+  // if statement for title and body to be filled out
+  // console.log(newIdeaObject);
+  renderIdeaCard()
 };
 
 // function showStarred() {
 // }
+// render to
+function renderIdeaCard() {
+  renderedIdeas.innerHTML += `<article class="idea-card">
+    <div class="idea-card-header">
+      <button class="unsaved-star">
+        <img class="star hidden" src="./assets/icons/star.svg" alt="empty star"/>
+        <img class= "star" src="./assets/icons/star-active.svg" alt="star"/>
+      </button>
+      <button class="img-button">
+        <img class="delete" src="./assets/icons/delete.svg"/>
+      </button>
+    </div>
+    <div class="idea-card-body">
+      <h3>${ideaTitle.value}</h3>
+      <p>${ideaBody.value}<p>
+    </div>
+    <div class="idea-card-footer">
+      <button class ="comment-button">
+        <img class="comment-image" src="./assets/icons/comment.svg" alt="comment image"/>
+      </button>
+      <label>Comment</label>
+    </div>
+  </article>`
+}
