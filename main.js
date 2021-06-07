9b6581b199a15b8cd48e96c474bc710686eee833
Justin Anthony <justincanthony@yahoo.com>
2021-06-06 19:48:13 -0600

    Update delete from storage functiion

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ var saveButton = document.querySelector('.save-btn');
 var displayStarredButton = document.querySelector('.show-starred-button');
 var addCommentButton = document.querySelector('.comment-button');
 var deleteIdeaButton = document.querySelector('.img-button')
-var ideaTitle = document.querySelector('.form-title'); // title text input
-var ideaBody = document.querySelector('.form-body'); // body text input
+var ideaTitle = document.querySelector('.form-title');
+var ideaBody = document.querySelector('.form-body');
 var searchText = document.querySelector('.search-field');
 var renderedIdeas = document.querySelector('.ideabox');
-var savedIdeas = [];
 
+var savedIdeas = [];
+console.log(savedIdeas);
 //👇 eventListeners👇
 saveButton.addEventListener('click', instantiateIdeaClass);
 
@@ -16,54 +17,16 @@ renderedIdeas.addEventListener('click', function(e) {
   if(e.target.className === 'delete') {
     deleteIdeaCard(e);
   };
+
   if(e.target.className === 'empty-star') {
     favoriteIdea(e);
   }
+
   if(e.target.className === 'star') {
     removeFavorite(e);
 }
 });
 
-// renderedIdeas.addEventListener('click', function(e) {
-//   if(e.target.className === 'delete'){
-//     var parent = e.target.parentElement;
-//     parent.parentNode.parentNode.remove();
-//   }
-//   if(e.target.className === "empty-star" || e.target.className === "star") {
-//     //new variable for article id
-//     var ideaArticle = e.target.parentElement.parentNode.parentNode;
-//     // idea article is equal to button
-//     for(var i = 0; i < savedIdeas.length; i++) {
-//       //if article id matches id in savedIdeas
-//       if(Number(ideaArticle.id) === savedIdeas[i].id) {
-//         console.log(e.target.className);
-//         console.log(e.target.nextElementSibling);
-//         //grab idea instance and update star property
-//
-//         // element we are updating.classList.add("hidden")
-//         if(savedIdeas[i].star === true) {
-//           //this happens when star is true
-//           savedIdeas[i].star = false;
-//           e.target.classList.add("hidden")
-//           e.target.nextElementSibling.classList.remove("hidden")
-//         } else {
-//           savedIdeas[i].star = true;
-//           // this happens when star is false
-//           e.target.classList.remove("hidden")
-//           e.target.previousElementSibling.classList.add("hidden")
-//         }
-//
-//         //remove sibling of target
-//         // value of starProperty === false
-//         console.log("working still!")
-//         // updating starValue to true
-//         console.log("final works");
-//
-//       }
-//     }
-//   }
-//   console.log(savedIdeas[0]);
-// })
 
 //👇 eventHandlers👇
 //Save card + make new instance of Idea
@@ -74,8 +37,10 @@ function instantiateIdeaClass(event) {
   } else {
     saveButton.disabled = false;
     var newIdeaObject = new Idea(ideaTitle.value, ideaBody.value);
+    console.log(newIdeaObject);
     savedIdeas.push(newIdeaObject);
     renderIdeaCard(newIdeaObject);
+    newIdeaObject.saveToStorage();
     // changeButton();
     // clearFields();
   }
@@ -104,49 +69,70 @@ function renderIdeaCard(newIdeaObject) {
       <label>Comment</label>
     </div>
   </article>`
-};
+}
 
 function deleteIdeaCard(e) {
   var parent = e.target.parentElement.parentNode.parentNode;
-  console.log(parent.id);
     for (i = 0; i < savedIdeas.length; i++) {
       if (Number(parent.id) === savedIdeas[i].id) {
+        savedIdeas[i].deleteFromStorage(savedIdeas[i].id);
         parent.remove();
         savedIdeas.splice(i, 1);
+
       }
     }
 };
+
 function favoriteIdea(e) {
-  var emptyStarImage = document.querySelector('.empty-star');
+  var emptyStarImage = e.target;
   var articleIdea = e.target.parentElement.parentNode.parentNode;
-  var favoritedStarImage = document.querySelector('.star');
+  var favoritedStarImage = e.target.nextElementSibling;
+
   for (var i = 0; i < savedIdeas.length; i++) {
    if (Number(articleIdea.id) === savedIdeas[i].id) {
      savedIdeas[i].star = true;
      favoritedStarImage.classList.remove('hidden');
      emptyStarImage.classList.add('hidden');
     }
-}
-}
+  }
+};
+
 function removeFavorite(e) {
-  var emptyStarImage = document.querySelector('.empty-star');
+  var emptyStarImage = e.target.previousElementSibling;
   var articleIdea = e.target.parentElement.parentNode.parentNode;
-  var favoritedStarImage = document.querySelector('.star');
+  var favoritedStarImage = e.target;
+
   for (var i = 0; i < savedIdeas.length; i++) {
     if (Number(articleIdea.id) === savedIdeas[i].id && savedIdeas[i].star === true) {
       savedIdeas[i].star = false;
-     favoritedStarImage.classList.add('hidden');
-     emptyStarImage.classList.remove('hidden');
-}
-}
-}
-
-function changeButton() {
-  saveButton.classList.remove('disable-button')
-  saveButton.classList.add('save-btn');
+      favoritedStarImage.classList.add('hidden');
+      emptyStarImage.classList.remove('hidden');
+    }
+  }
 };
 
-function clearFields() {
-  ideaTitle.value = "";
-  ideaBody.value = "";
-};
+// function saveToStorage(newIdeaObject) {
+//   var storageId = articleIdea.id;
+//   console.log(storageId);
+//   var stringifiedIdea = JSON.stringify(newIdeaObject);
+//   localStorage.setItem(storageId, stringifiedIdea)
+// };
+
+
+// 👇 Unfinished Functions👇
+// disableButton() {
+//saveButton.disabled = true;
+//}
+//
+// function showStarred() {
+// }
+//
+// function changeButton() {
+//   saveButton.classList.remove('disable-button')
+//   saveButton.classList.add('save-btn');
+// }
+//
+// function clearFields() {
+//   ideaTitle.value = "";
+//   ideaBody.value = "";
+// }
